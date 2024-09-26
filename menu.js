edecad3c306fd919867346112038f1fac82509df
Brayan Martínez <zs22017044@estudiantes.uv.mx>
2024-09-26 00:52:53 -0600

    Responsive nav

--- menu.js
+++ menu.js
@@ -2,7 +2,7 @@ const menuToggle = document.querySelector('.menu-toggle');
 const navMenu = document.querySelector('.nav-menu');
 const menuLinks = document.querySelectorAll('.nav-menu li a');
 
-// Alternar menú al hacer clic en el icono de hamburguesa
+// Alternar el menú al hacer clic en el icono de hamburguesa
 menuToggle.addEventListener('click', () => {
     navMenu.classList.toggle('active');
     menuToggle.classList.toggle('active');
@@ -16,7 +16,7 @@ menuLinks.forEach(link => {
     });
 });
 
-// Cerrar el menú al hacer clic fuera del menú
+// Cerrar el menú si se hace clic fuera del menú
 document.addEventListener('click', function(event) {
     if (!event.target.closest('.nav-menu') && !event.target.closest('.menu-toggle')) {
         navMenu.classList.remove('active');
